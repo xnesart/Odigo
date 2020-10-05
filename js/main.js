c1e503ffb87e5784c2428b448b9f6b31b6b9d691
xnesart <60525875+xnesart@users.noreply.github.com>
2020-10-05 11:04:32 +0300

    nav menu mobile

--- js/main.js
+++ js/main.js
@@ -8,3 +8,17 @@
         }
     }
 }());
+
+// BURGER MENU
+(function () {
+    const burgerItem = document.querySelector('.burger');
+    const menu = document.querySelector('.nav');
+    const menuCloseItem = document.querySelector('.nav-close');
+burgerItem.addEventListener('click', () => {
+    menu.classList.add('nav_active');
+});
+    menuCloseItem.addEventListener('click', () => {
+        menu.classList.remove('nav_active');
+    });
+
+}());
